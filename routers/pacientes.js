048c364924a23aa1bca743f8e1abc33ffa1b482e
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-07-12 13:28:16 -0500

    feat(EndPoint): :sparkles: Creacion endPoint obtener pacientes que tienen cita con dicho doctor
    se creo el endPoint que me trae los datos de los pacientes que tiene cita con un mismo doctor
    
    by:Carlos Villafrades

--- routers/pacientes.js
+++ routers/pacientes.js
@@ -26,5 +26,23 @@ storagePacientes.get("/", (req,res)=>{
     )
 })
 
+storagePacientes.get("/medico/:idMedico", (req,res)=>{
+
+const {idMedico} = req.params;
+
+    con.query(
+        /*sql*/ `SELECT usuario.*, cita.cit_fecha, medico.med_nroMatriculaProfesional, medico.med_nombreCompleto FROM usuario INNER JOIN cita ON usuario.usu_id = cita.cit_datosUsuario INNER JOIN medico ON cita.cit_medico = medico.med_nroMatriculaProfesional WHERE medico.med_nroMatriculaProfesional = ?`,
+        [idMedico],
+        
+        (err,data,fil)=>{
+            if (err) {
+                res.status(500).send("Error al obtener los datos")
+            }else{
+                res.send(data)
+            }   
+        }
+        )
+})
+
 
 export default storagePacientes;
